07e0a88f8ef66b4ba85185ca0a11b4d15809b09e
MEDaymaneazarkale <115282981+MEDaymaneazarkale@users.noreply.github.com>
2023-03-06 23:01:42 +0100

    Add files via upload

---
+++ Back.js
@@ -0,0 +1,67 @@
+(function($) { "use strict";
+
+	$(document).ready(function(){"use strict";
+		
+		var progressPath = document.querySelector('.progress-wrap path');
+		var pathLength = progressPath.getTotalLength();
+		progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
+		progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
+		progressPath.style.strokeDashoffset = pathLength;
+		progressPath.getBoundingClientRect();
+		progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';		
+		var updateProgress = function () {
+			var scroll = $(window).scrollTop();
+			var height = $(document).height() - $(window).height();
+			var progress = pathLength - (scroll * pathLength / height);
+			progressPath.style.strokeDashoffset = progress;
+		}
+		updateProgress();
+		$(window).scroll(updateProgress);	
+		var offset = 50;
+		var duration = 550;
+		jQuery(window).on('scroll', function() {
+			if (jQuery(this).scrollTop() > offset) {
+				jQuery('.progress-wrap').addClass('active-progress');
+			} else {
+				jQuery('.progress-wrap').removeClass('active-progress');
+			}
+		});				
+		jQuery('.progress-wrap').on('click', function(event) {
+			event.preventDefault();
+			jQuery('html, body').animate({scrollTop: 0}, duration);
+			return false;
+		})
+		
+		
+	});
+	
+})(jQuery);
+
+// navigation bar scrolling mechanisme
+const button = document.getElementById("button");
+const slide = document.getElementById("slidebar");
+const navBar = document.getElementById("navBar");
+let switcher = true;
+
+button.addEventListener("click",function me(){
+	if (switcher == false) {
+		switcher = true;
+		slide.style.marginRight = "-550px";
+	}
+	else if (switcher == true) {
+		switcher = false;
+		slide.style.marginRight = "0px";
+	}
+});
+
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
